e3045857038d952d90566f242963d4d236a87476
Christopher Perry <ozbonus@gmail.com>
2019-05-27 00:15:30 +0800

    Slow down turning significantly, add dance

--- pygame-controller.py
+++ pygame-controller.py
@@ -28,8 +28,6 @@ right_backward_pwm = GPIO.PWM(15, 100)
 # ############ #
 
 pygame.init()
-# window = pygame.display.set_mode((100, 100))
-
 os.environ['SDL_VIDEODRIVER'] = 'dummy'
 pygame.display.init()
 screen = pygame.display.set_mode((100, 100))
@@ -38,21 +36,45 @@ screen = pygame.display.set_mode((100, 100))
 # Controller Logic #
 # ################ #
 
-def forward(duty=100):
+def forward(duty=100, seconds=None):
     left_forward_pwm.start(duty)
     right_forward_pwm.start(duty)
+    if seconds:
+        time.sleep(seconds)
+        stop()
+        time.sleep(0.2)
 
-def backward(duty=100):
+def backward(duty=100, seconds=None):
     left_backward_pwm.start(duty)
     right_backward_pwm.start(duty)
+    if seconds:
+        time.sleep(seconds)
+        stop()
+        time.sleep(0.2)
 
-def left(duty=60):
-    right_forward_pwm.start(duty)
-    left_backward_pwm.start(duty)
+def left(duty=55, seconds=None):
+    # Cheap motors require a little push at full power.
+    right_forward_pwm.start(100)
+    left_backward_pwm.start(100)
+    time.sleep(0.1)
+    right_forward_pwm.ChangeDutyCycle(duty)
+    left_backward_pwm.ChangeDutyCycle(duty)
+    if seconds:
+        time.sleep(seconds)
+        stop()
+        time.sleep(0.2)
 
-def right(duty=60):
-    right_backward_pwm.start(duty)
-    left_forward_pwm.start(duty)
+def right(duty=55, seconds=None):
+    # Cheap motors require a little push at full power.
+    right_backward_pwm.start(100)
+    left_forward_pwm.start(100)
+    time.sleep(0.1)
+    right_backward_pwm.ChangeDutyCycle(duty)
+    left_forward_pwm.ChangeDutyCycle(duty)
+    if seconds:
+        time.sleep(seconds)
+        stop()
+        time.sleep(0.2)
 
 def stop():
     left_backward_pwm.stop()
@@ -60,17 +82,27 @@ def stop():
     right_forward_pwm.stop()
     right_backward_pwm.stop()
 
+def dance():
+    forward(seconds=0.5)
+    forward(seconds=0.5)
+    backward(seconds=0.5)
+    backward(seconds=0.5)
+    left(duty=100, seconds=0.5)
+    right(duty=100, seconds=1.5)
+    right(duty=100, seconds=0.5)
+    left(duty=100, seconds=1.5)
+
 running = True
 
 while running:
-    pygame.time.delay(100)
+    pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYUP:
             stop()
         if event.type == pygame.KEYDOWN:
-            if event.key ==pygame.K_SPACE:
+            if event.key == pygame.K_ESCAPE:
                 GPIO.cleanup()
                 pygame.quit()
             if event.key == pygame.K_UP:
@@ -81,7 +113,8 @@ while running:
                 left()
             if event.key == pygame.K_RIGHT:
                 right()
-        
+            if event.key == pygame.K_SPACE:
+                dance()
 
 # ######## #
 # Clean Up #
